e609c51055c51768d7a4c7aff6f3521ce894bfc7
jeevesh2002 <jeevesh2002@users.noreply.github.com>
2021-06-02 16:19:52 +0000

    automated commit by submit50

--- heredity.py
+++ heredity.py
@@ -4,7 +4,6 @@ import sys
 
 PROBS = {
 
-    # Unconditional probabilities for having gene
     "gene": {
         2: 0.01,
         1: 0.03,
@@ -13,38 +12,32 @@ PROBS = {
 
     "trait": {
 
-        # Probability of trait given two copies of gene
         2: {
             True: 0.65,
             False: 0.35
         },
 
-        # Probability of trait given one copy of gene
         1: {
             True: 0.56,
             False: 0.44
         },
 
-        # Probability of trait given no gene
         0: {
             True: 0.01,
             False: 0.99
         }
     },
 
-    # Mutation probability
     "mutation": 0.01
 }
 
 
 def main():
 
-    # Check for proper usage
     if len(sys.argv) != 2:
         sys.exit("Usage: python heredity.py data.csv")
     people = load_data(sys.argv[1])
 
-    # Keep track of gene and trait probabilities for each person
     probabilities = {
         person: {
             "gene": {
@@ -60,11 +53,9 @@ def main():
         for person in people
     }
 
-    # Loop over all sets of people who might have the trait
     names = set(people)
     for have_trait in powerset(names):
 
-        # Check if current set of people violates known information
         fails_evidence = any(
             (people[person]["trait"] is not None and
              people[person]["trait"] != (person in have_trait))
@@ -73,18 +64,14 @@ def main():
         if fails_evidence:
             continue
 
-        # Loop over all sets of people who might have the gene
         for one_gene in powerset(names):
             for two_genes in powerset(names - one_gene):
 
-                # Update probabilities with new joint probability
                 p = joint_probability(people, one_gene, two_genes, have_trait)
                 update(probabilities, one_gene, two_genes, have_trait, p)
 
-    # Ensure probabilities sum to 1
     normalize(probabilities)
 
-    # Print results
     for person in people:
         print(f"{person}:")
         for field in probabilities[person]:
@@ -95,12 +82,7 @@ def main():
 
 
 def load_data(filename):
-    """
-    Load gene and trait data from a file into a dictionary.
-    File assumed to be a CSV containing fields name, mother, father, trait.
-    mother, father must both be blank, or both be valid names in the CSV.
-    trait should be 0 or 1 if trait is known, blank otherwise.
-    """
+
     data = dict()
     with open(filename) as f:
         reader = csv.DictReader(f)
@@ -117,9 +99,7 @@ def load_data(filename):
 
 
 def powerset(s):
-    """
-    Return a list of all possible subsets of set s.
-    """
+
     s = list(s)
     return [
         set(s) for s in itertools.chain.from_iterable(
@@ -129,15 +109,7 @@ def powerset(s):
 
 
 def joint_probability(people, one_gene, two_genes, have_trait):
-    """
-    Compute and return a joint probability.
-    The probability returned should be the probability that
-        * everyone in set `one_gene` has one copy of the gene, and
-        * everyone in set `two_genes` has two copies of the gene, and
-        * everyone not in `one_gene` or `two_gene` does not have the gene, and
-        * everyone in set `have_trait` has the trait, and
-        * everyone not in set` have_trait` does not have the trait.
-    """
+
     probability = 1
 
     for person in people:
@@ -177,12 +149,7 @@ def joint_probability(people, one_gene, two_genes, have_trait):
 
 
 def update(probabilities, one_gene, two_genes, have_trait, p):
-    """
-    Add to `probabilities` a new joint probability `p`.
-    Each person should have their "gene" and "trait" distributions updated.
-    Which value for each distribution is updated depends on whether
-    the person is in `have_gene` and `have_trait`, respectively.
-    """
+
     for person in probabilities:
         gene_number = 1 if person in one_gene else 2 if person in two_genes else 0
         probabilities[person]["gene"][gene_number] += p
@@ -190,10 +157,7 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
 
 
 def normalize(probabilities):
-    """
-    Update `probabilities` such that each probability distribution
-    is normalized (i.e., sums to 1, with relative proportions the same).
-    """
+
     normalized = probabilities.copy()
     for person in probabilities:
         for typ in ['gene', 'trait']:
